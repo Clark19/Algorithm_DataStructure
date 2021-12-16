950cdddc255d0cd8831e7bd04383ec574ac6d946
Clark19 <clarkgable1@gmail.com>
2021-12-16 23:03:54 +0900

    소수 구하는 함수 : 에라토스테네스의 체

---
+++ 모두의 알고리즘 with 파이썬/general.py
@@ -0,0 +1,14 @@
+# 소수 구하는 함수 : 에라토스테네스의 체
+def eratosthenes(num):
+  # sieve = [1 for i in range(num)] # 1로 표시된 것들의 인덱스만 소수임
+  sieve = [1] * num # 1로 표시된 것들의 인덱스만 소수임
+  sieve[0], sieve[1] = 0, 0
+  for n in range(2, num):
+    for j in range(2*n, num, n):
+      sieve[j] = 0
+  
+  # return [idx for idx, value in enumerate(sieve) if value==True]
+  # return list(filter(lambda idx: sieve[idx]==1, range(2,n+1)))
+  return [idx for idx in range(2,n+1) if sieve[idx]==True]
+print(eratosthenes(10))
+print(eratosthenes(20))
