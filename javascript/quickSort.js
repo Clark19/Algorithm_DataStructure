3f413f004d7ab4706b9f1dee167ab463f32a2901
Clark19 <clarkgable1@gmail.com>
2022-09-17 23:59:16 +0900

    add: assert 이용방식 테스트 추가

--- javascript/quickSort.js
+++ javascript/quickSort.js
@@ -99,4 +99,35 @@ function getPivot(arr, leftIdx, rightIdx) {
   return { value: arrToSort[1].value, idx: arrToSort[1].idx };
 }
 
+
+// const assert = requre("assert")
+
+// const testData = [
+//   { input: [3, 1, 2, 5, 4], answer: [1, 2, 3, 4, 5] },
+//   { input: [3, 6, 1, 3, 2, 5, 4], answer: [1, 2, 3, 3, 4, 5, 6] },
+//   { input: [3, 1, 3, 2, 5, 4], answer: [1, 2, 3, 3, 4, 5] },
+//   { input: [2, 2, 2], answer: [2, 2, 2] },
+//   { input: [3, 1, 2], answer: [1, 2, 3] },
+//   { input: [-3, -1, -2], answer: [-3, -2, -1] },
+//   {
+//     input: [5, 7, 9, 0, 3, 1, 6, 2, 4, 8],
+//     answer: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+//   },
+//   {
+//     input: [100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23],
+//     answer: [-3, 1, 2, 2, 3, 4, 5, 6, 9, 23, 100],
+//   },
+// ];
+
+
+// let output = null
+// testData.forEach(data => {
+//   output = qsort(data.input)
+//   console.log(output)
+//   assert.deepStrictEqual(output, data.answer)
+// });
+
+// console.log("good")
+
+
 module.exports = quickSort;
