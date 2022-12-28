e95c80126e8a8fc19ac5abab0ca74ee0001056fd
Cark19 <clarkgable1@gmail.com>
2022-12-29 00:01:47 +0900

    백준 2798번 블랙잭, 1000번 : Javascript

--- BackJoon/Solved.ac.C1.js
+++ BackJoon/Solved.ac.C1.js
@@ -1,5 +1,16 @@
-//  2920번 음계 - https://www.acmicpc.net/problem/2920
+// 백준 알고리즘 JavaScript 로 내가 푼 코드들
 
+// 1000번 A+B - https://www.acmicpc.net/problem/1000
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map((el) => Number(el));
+
+console.log(input[0] + input[1]);
+
+//  2920번 음계 - https://www.acmicpc.net/problem/2920
 // 방법 1 - 직관적 방법 forEach
 {
   const fs = require("fs");
@@ -39,4 +50,25 @@
   };
 
   console.log(obj[input] || "mixed");
+}
+
+// 블랙잭 - https://www.acmicpc.net/problem/2798
+// 방법 1 - 3중 for문
+{
+  const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+  const [N, M] = input[0].split(" ").map(Number);
+  const data = input[1].split(" ").map(Number);
+
+  let result = 0;
+  for (let i = 0; i < N; i++) {
+    for (let j = i + 1; j < N; j++) {
+      for (let k = j + 1; k < N; k++) {
+        sum = data[i] + data[j] + data[k];
+        if (sum <= M) {
+          result = sum > result ? sum : result;
+        }
+      }
+    }
+  }
+  console.log(result);
 }
