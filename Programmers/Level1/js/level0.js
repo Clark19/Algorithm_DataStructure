8973acaa9c581c72abb4b67b99c0c88cb7a7a95f
Cark19 <clarkgable1@gmail.com>
2023-01-12 23:59:20 +0900

    프로그래머스 레벨 0 자바스크립트로 푼거 4일차 // 피자 나눠 먹기 (1)

---
+++ Programmers/Level1/js/level0.js
@@ -0,0 +1,8 @@
+/** 프로그래머스 레벨 0 자바스크립트로 푼거 */
+
+// 4일차
+// 피자 나눠 먹기 (1) - https://school.programmers.co.kr/learn/courses/30/lessons/120814
+function solution(n) {
+  var answer = 0;
+  return answer;
+}
