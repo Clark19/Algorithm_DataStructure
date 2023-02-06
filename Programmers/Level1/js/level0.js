1ca4dca4832b38111bd0c456276c53e2859d4d7d
Cark19 <clarkgable1@gmail.com>
2023-02-06 22:02:10 +0900

    프로그래머스 레벨 0 자바스크립트 8일차 '순서쌍의 개수' 품

--- Programmers/Level1/js/level0.js
+++ Programmers/Level1/js/level0.js
@@ -203,3 +203,22 @@ function solution(emergency) {
   let sortedEmergecy = emergency.slice().sort((a, b) => b - a);
   return emergency.map((n) => sortedEmergecy.indexOf(n) + 1);
 }
+
+// 순서쌍의 개수 https://school.programmers.co.kr/learn/courses/30/lessons/120836?language=javascript
+function solution(n) {
+  // var answer = 0
+  // for (let i=1; i<=n; i++) {
+  //     for (let j=1; j<=n; j++) {
+  //         if (i*j === n)
+  //             answer++
+  //     }
+  // }
+  // return answer;
+
+  let answer = 0;
+  for (let i = 2; i <= n; i++) {
+    if (n % i === 0) answer++;
+  }
+
+  return ++answer;
+}
