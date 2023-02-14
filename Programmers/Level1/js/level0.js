d867b8cc24ca0a8bf4dddf5c68138c1d73a0f24b
Clark19 <clarkgable1@gmail.com>
2023-02-14 23:04:38 +0900

    프로그래머스: 레벨0 Day11 1번 품

--- Programmers/Level1/js/level0.js
+++ Programmers/Level1/js/level0.js
@@ -350,3 +350,12 @@ function solution(numbers, direction) {
   }
   return numbers;
 }
+
+// Day 11 수학, 반복문
+// 주사위의 개수 https://school.programmers.co.kr/learn/courses/30/lessons/120845?language=javascript
+function solution(box, n) {
+  let answer = Math.floor(box[0] / n);
+  answer *= Math.floor(box[1] / n);
+  answer *= Math.floor(box[2] / n);
+  return answer;
+}
