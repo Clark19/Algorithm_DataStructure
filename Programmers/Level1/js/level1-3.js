9e010fb64cfb08dd14e787d8aba46f14d5238534
Clark19 <clarkgable1@gmail.com>
2023-02-25 14:55:05 +0900

    프로그래머스: 레벨1 '문자열 내 마음대로 정렬하기'

---
+++ Programmers/Level1/js/level1-3.js
@@ -0,0 +1,6 @@
+// 문자열 내 마음대로 정렬하기 https://school.programmers.co.kr/learn/courses/30/lessons/12915
+function solution(strings, n) {
+  return [...strings].sort((a, b) =>
+    a[n] === b[n] ? a.localeCompare(b) : a[n].charCodeAt() - b[n].charCodeAt()
+  );
+}
