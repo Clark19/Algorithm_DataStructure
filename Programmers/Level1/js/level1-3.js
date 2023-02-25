c0479c6eca67c14fa63c73069645a9a46ac524f0
Clark19 <clarkgable1@gmail.com>
2023-02-25 15:10:34 +0900

    프로그래머스: 레벨1 '크기가 작은 부분문자열'

--- Programmers/Level1/js/level1-3.js
+++ Programmers/Level1/js/level1-3.js
@@ -4,3 +4,14 @@ function solution(strings, n) {
     a[n] === b[n] ? a.localeCompare(b) : a[n].charCodeAt() - b[n].charCodeAt()
   );
 }
+
+// 크기가 작은 부분문자열 https://school.programmers.co.kr/learn/courses/30/lessons/147355
+// 문자열에도 slice() 사용 가능.
+function solution(t, p) {
+  const answer = [];
+  for (let i = 0; i <= t.length - p.length; i++) {
+    answer.push(t.slice(i, i + p.length));
+  }
+  const pNum = parseInt(p);
+  return answer.filter((nStr) => parseInt(nStr) <= pNum).length;
+}
