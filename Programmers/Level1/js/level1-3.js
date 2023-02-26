b53e381778754e44884a1bca67304d931457ad7a
Clark19 <clarkgable1@gmail.com>
2023-02-26 23:59:20 +0900

    프로그래머스: 레벨1 품

--- Programmers/Level1/js/level1-3.js
+++ Programmers/Level1/js/level1-3.js
@@ -15,3 +15,53 @@ function solution(t, p) {
   const pNum = parseInt(p);
   return answer.filter((nStr) => parseInt(nStr) <= pNum).length;
 }
+
+
+// 콜라 문제 https://school.programmers.co.kr/learn/courses/30/lessons/132267
+function solution(a, b, n) {
+  // 방식1: 처음 푼 방식
+//     let acc = ~~(n/a)*b;//6
+//     let margin = n%a;//2
+//     let curBottles = ~~(n/a)*b + margin;//8
+  
+//     while (curBottles >= a) {
+//         acc += ~~(curBottles/a)*b;//8
+//         margin = curBottles%a;//2
+//         curBottles = ~~(curBottles/a)*b + margin
+//     }
+  
+//     return acc;
+  
+  // 방식2: 방식1의 로직을 더 단순화
+  let acc = 0;    
+  while (n >= a) {
+      acc += ~~(n/a)*b;//8
+      n = ~~(n/a)*b + n%a
+  }
+  return acc;
+}
+
+// 푸드 파이트 대회 https://school.programmers.co.kr/learn/courses/30/lessons/134240
+function solution(food) {
+  let rst = ""
+  food.forEach((n,i) => {
+      // for (let j=0; j<Math.floor(n/2); j++)
+      //     rst += i
+      rst += i.toString().repeat(Math.floor(n/2))
+  })
+
+  return rst + "0" + [...rst].reverse().join("");
+}
+
+// 소수 찾기 https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=javascript#
+function solution(n) {
+  const eratos = Array(n+1).fill(1);
+  eratos[0]=0, eratos[1]=0;
+  for (let i=2; i*i<=n; i++) {
+  // for (let i=2; i<=Math.sqrt(n); i++) {
+
+      for (let j=i*i; j<=n; j += i)
+          eratos[j] = 0;
+  }
+  return eratos.filter(v => v).length;
+}
--- 모두의 알고리즘 with 파이썬/general.py
+++ 모두의 알고리즘 with 파이썬/general.py
@@ -1,10 +1,13 @@
+import math
+
+
 # 소수 구하는 함수 : 에라토스테네스의 체
 def eratosthenes(num):
   # sieve = [1 for i in range(num)] # 1로 표시된 것들의 인덱스만 소수임
   sieve = [1] * num # 1로 표시된 것들의 인덱스만 소수임
   sieve[0], sieve[1] = 0, 0
-  for i in range(2, num):
-    for j in range(2*i, num, i):
+  for i in range(2, int(math.sqrt(num))):
+    for j in range(i*i, num, i):
       sieve[j] = 0
   
   # return [idx for idx, value in enumerate(sieve) if value==True]
