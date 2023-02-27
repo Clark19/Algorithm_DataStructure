64bd82b4895653a02da1ca85003ce21a9c5a507e
Clark19 <clarkgable1@gmail.com>
2023-02-28 00:21:01 +0900

    프로그래머스: 레벨1 4문제 품

--- Programmers/Level1/js/level1-3.js
+++ Programmers/Level1/js/level1-3.js
@@ -65,3 +65,90 @@ function solution(n) {
   }
   return eratos.filter(v => v).length;
 }
+
+// 가장 가까운 같은 글자 https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=javascript
+function solution(s) {
+  const lastIdxMap = {};
+  const arr = s.split("").map((ch, curIdx) => {
+      const rst = lastIdxMap[ch] == undefined ? -1 : curIdx-lastIdxMap[ch];
+      lastIdxMap[ch] = curIdx;
+      return rst;
+  });
+
+  return arr;
+}
+
+// 실패율 - https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=javascript
+// 2019 KAKAO BLIND RECRUITMENT
+function solution(N, stages) {
+  // 방식1. 처음 푼 방식: 빠른 방식. 코드가 복잡하지만 시간 복잡도가 O(N) 이라서 방식2( O(N^2) ) 보다 빠름. 램 용량도 적게 사용.
+  const failStagesOrder = Array(N+1).fill();
+  stages.forEach((stg, i) => {
+      if (failStagesOrder[stg-1] == undefined)
+          failStagesOrder[stg-1] = [stg, 0];
+      failStagesOrder[stg-1][1]++;
+  });
+  
+  let restUsers = stages.length;
+  let usersPerStg = 0;
+  for (let i=0; i<failStagesOrder.length-2; i++) {
+      if (failStagesOrder[i] == undefined) {
+          failStagesOrder[i] = [i+1, 0]
+          continue
+      }
+      usersPerStg = failStagesOrder[i][1];
+      failStagesOrder[i][1] /= restUsers; // 실패율 계산후, 배열의 값을 유저수에서 실패율로 변경.
+      restUsers -= usersPerStg
+  }
+  // 마지막 스테이지 예외 처리.
+  if (failStagesOrder[failStagesOrder.length-2] == undefined)
+      failStagesOrder[failStagesOrder.length-2] = [N, 0];
+  if (failStagesOrder[failStagesOrder.length-1] == undefined)
+      failStagesOrder[failStagesOrder.length-1] = [N+1, 0];
+  failStagesOrder[failStagesOrder.length-2][1] = failStagesOrder.at(-2)[1] / (failStagesOrder.at(-2)[1] + failStagesOrder.at(-1)[1]);
+  failStagesOrder.pop()
+  
+  return failStagesOrder.sort((a,b) => b[1]-a[1]).map(el => el[0])
+  
+  
+  // 방식2: 코드 작성 쉽고 가독성 좋아서 빠르게 풀어서 코테에서 시간 절약시엔 좋음.
+  // 시간 복잡도는 안좋음: O(N^2) 이라서 느림 - 중첩 루프(for 안에 filter()) 라서.
+  // 프로그래머스 상에서 방식1이 최악성능은 약 7ms 인데, 이 방식은 최대 약 4728ms라서 600배 이상 느림.
+//     const rst = []
+//     for (let i=1; i<=N; i++) {
+//         const reaches = stages.filter(v => v>=i).length;
+//         const fail = stages.filter(v => v === i).length;
+//         rst.push([i, fail/reaches])
+//     };
+
+//     return rst.sort((a,b) => b[1]-a[1]).map(v => v[0])
+}
+
+// 과일 장수 - https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=javascript
+function solution(k, aplPerBox, score) {
+  score.sort((a,b) => b-a)
+  const numOfBox = Math.floor(score.length/aplPerBox);
+  const boxes = Array(numOfBox).fill(0).map(el => []);
+  for (let i=0; i<numOfBox*aplPerBox; i++) {
+     boxes[Math.floor(i/aplPerBox)].push(score[i]);
+  };
+  return boxes.reduce((acc, box) => box[aplPerBox-1]*aplPerBox + acc, 0);
+}
+
+// 명예의 전당 (1) https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=javascript
+function solution(k, score) {
+  const ranking3 = [];
+  const thirdList = [];
+  score.forEach((v,i) => {
+      if (i < k) {
+          ranking3.push(v); 
+          ranking3.sort((a,b) => b-a);
+      }
+      else if (v > ranking3.at(-1) ) {
+          ranking3[ranking3.length-1] = v;
+          ranking3.sort((a,b) => b-a);
+      }
+      thirdList.push(ranking3.at(-1));              
+  });
+  return thirdList;
+}
