82f8adc162f211df15f1eeff4653c4a87369aadd
Clark19 <clarkgable1@gmail.com>
2023-04-11 23:57:58 +0900

    유데미: JS 알고리즘 & 자료구조 - 슬라이딩 윈도우 2문제 품

--- javascript/Udemy_js_algo/FrequencyCounter.js
+++ javascript/Udemy_js_algo/FrequencyCounter.js
@@ -339,3 +339,82 @@ function maxSubarraySum(intArr, num){
   console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
   console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
   console.log(maxSubarraySum([2,3], 3)) // null
+
+
+/* 
+Sliding Window - minSubArrayLen
+ - https://coupang.udemy.com/course/best-javascript-data-structures/learn/quiz/5338805#learning-tools
+Write a function called minSubArrayLen which accepts two parameters
+ - an array of positive integers and a positive integer.
+
+ This function should return the minimal length of a contiguous subarray
+of which the sum is greater than or equal to the integer passed to the function.
+If there isn't one, return 0 instead.
+
+Time Complexity - O(n)
+Space Complexity - O(1)
+*/
+function minSubArrayLen(nums, sum) {
+  let total = 0
+  let start = 0
+  let end = 0
+  let minLen = Infinity
+
+  while (start < nums.length) {
+    if (total < sum && end < nums.length) {
+      total += nums[end++]
+    } else if (total >= sum) {
+      minLen = Math.min(minLen, end - start)
+      total -= nums[start++]
+    } else 
+      break
+  }
+
+  return minLen === Infinity ? 0 : minLen
+}
+console.log(minSubArrayLen([2,3,1,2,4,3], 7)) // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2,1,6,5,4], 9)) // 2 -> because [5,4] is the smallest subarray
+console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)) // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
+
+
+/* 
+Sliding Window - findLongestSubstring
+Write a function called findLongestSubstring, which accepts a string
+and returns the length of the longest substring with all distinct characters.
+
+Time Complexity - O(n)
+*/
+function findLongestSubstring(str){
+  // add whatever parameters you deem necessary - good luck!
+  let maxLen = -Infinity
+  let start = 0
+  let end = 0
+  let arr = []
+  let idx = -1
+  while (end < str.length) {
+    idx = arr.indexOf(str[end])
+    if (idx === -1 && end === str.length - 1) {
+      arr.push(str[end++])
+      maxLen = Math.max(maxLen, arr.length)
+    } else if (idx >= -1) {
+      maxLen = Math.max(maxLen, arr.length)
+      start = idx
+      arr.splice(0, start + 1)
+    }
+    arr.push(str[end++])
+  }
+
+  return maxLen === -Infinity ? str.length : maxLen;
+}
+
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('rithmschool')) // 7
+console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thecatinthehat')) // 7
+console.log(findLongestSubstring('bbbbbb')) // 1
+console.log(findLongestSubstring('longestsubstring')) // 8
+console.log(findLongestSubstring('thisishowwedoit')) // 6
