594c4b7e1c3c2b2c29e33807a7775b7a8095c460
Clark19 <clarkgable1@gmail.com>
2023-04-24 23:25:44 +0900

    유데미: JS 알고리즘 & 자료구조 - (optimized) Bubble Sort 구현

--- javascript/Udemy_js_algo/search.js
+++ javascript/Udemy_js_algo/search.js
@@ -76,3 +76,22 @@ function searchNaiveStr(str, target) {
 
   return cnt
 }
+
+
+/* (optimized) Bubble Sort */
+function bubbleSort(arr) {
+  let isSwap = false
+  for (let i=arr.length; i>0; i--) {
+    isSwap = false
+    for (let j=0; j<i-1; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+        isSwap = true
+      }
+    }
+    if (!isSwap) break
+  }
+
+  return arr
+}
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))
